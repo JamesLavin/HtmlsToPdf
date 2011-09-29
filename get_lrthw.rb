a437340b0aa6d7fef12f8a95f3387553ce167a9b
James Lavin <james@jameslavin.com>
2011-09-29 19:11:13 -0400

    refactor; add Python book

--- get_lrthw.rb
+++ get_lrthw.rb
@@ -2,9 +2,93 @@ require 'rubygems'
 require 'fileutils'
 require 'pdfkit'
 
-savedir = ARGV[0] ? ARGV[0] || '~/Ruby_programs/Learn_Ruby_the_Hard_Way'
+PDFKit.configure do |config|
+  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+end
 
-def build_urls
+class HtmlsToPdf
+
+  attr_reader :hfarray, :pdfarray, :cssarray, :urls, :savedir, :savename
+
+  def initialize(savedir, savename, urls_array, css_array = [], remove_temp_files = true)
+    set_dir(savedir)
+    @savename = savename
+    @urls = urls_array
+    @hfarray = urls_array.map { |url| File.basename(url) }
+    @pdfarray = @hfarray.map { |html_file| html_file.sub(/\.html?$/,'.pdf') }
+    @cssarray = css_array
+  end
+
+  def set_dir(savedir)
+    @savedir = savedir
+    save_to = File.expand_path(savedir)
+    FileUtils.mkdir_p(save_to)
+    Dir.chdir(save_to)
+  end
+
+  #def add_css(css_file)
+  #  @cssarray << css_file
+  #end
+
+  def download_files
+    download_html_files
+    download_css_files
+  end
+
+  def download_html_files
+    existing_files = Dir.entries(".")
+    @urls.each do |url|
+      savename = File.basename(url)
+      unless existing_files.include?(savename)
+        `wget #{url}`
+      end
+    end
+  end
+
+  def download_css_files
+    existing_files = Dir.entries(".")
+    @cssarray.each do |css_url|
+      `wget #{css_url}` unless existing_files.include?(File.basename(css_url))
+    end
+  end
+
+  def generate_pdfs
+    @hfarray.each_with_index { |html_file,i| html_to_pdf(html_file,@pdfarray[i]) }
+  end
+
+  def html_to_pdf(html_file,pdf_file)
+    html = nil
+    unless Dir.entries(".").include?(pdf_file)
+      File.open(html_file, 'r') { |inf| html = inf.read }
+      kit = PDFKit.new(html, :page_size => 'Letter')
+      @cssarray.each { |cssfile| kit.stylesheets << File.basename(cssfile) }
+      kit.to_file(pdf_file)
+    end
+  end
+
+  def join_pdfs
+    unless File.exists?(@savename)
+      pdfs_string = @pdfarray.join(" ")
+      `pdftk #{pdfs_string} output #{@savename}`
+    end
+  end
+
+  def delete_temp_files
+    @pdfarray.each { |pdffile| File.delete(pdffile) }
+    @hfarray.each { |htmlfile| File.delete(htmlfile) }
+    @cssarray.each { |cssfile| File.delete(File.basename(cssfile)) }
+  end
+
+  def create_pdf
+    download_files
+    generate_pdfs
+    join_pdfs
+    delete_temp_files
+  end
+
+end
+
+def ruby_hard_way_urls
   urls = ['http://ruby.learncodethehardway.org/book/intro.html']
   (1..52).each do |val|
     urls << 'http://ruby.learncodethehardway.org/book/ex' + val.to_s.rjust(2,'0') + '.html'
@@ -14,74 +98,29 @@ def build_urls
   urls
 end
 
-PDFKit.configure do |config|
-  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
-end
-
-def set_dir(savedir)
-  save_to = File.expand_path(SAVEDIR)
-  FileUtils.mkdir_p(save_to)
-  Dir.chdir(save_to)
-end
-
-def get_html_files(html_urls)
-  existing_files = Dir.entries(".")
-  html_urls.each do |url|
-    savename = File.basename(url)
-    unless existing_files.include?(savename)
-      `wget #{url}`
-    end
+def python_hard_way_urls
+  urls = ['http://ruby.learncodethehardway.org/book/intro.html']
+  (0..52).each do |val|
+    urls << 'http://learnpythonthehardway.org/book/ex' + val.to_s + '.html'
   end
+  urls << 'http://learnpythonthehardway.org/book/next.html'
+  urls << 'http://learnpythonthehardway.org/book/advice.html'
+  urls
 end
 
-def get_css_files
-  existing_files = Dir.entries(".")
-  `wget http://ruby.learncodethehardway.org/book/css/960gs/960.css` unless existing_files.include?("960.css")
-  `wget http://ruby.learncodethehardway.org/book/css/screen.css` unless existing_files.include?('screen.css')
-  `wget http://ruby.learncodethehardway.org/book/css/syntax.css` unless existing_files.include?('syntax.css')
-end
+# Ruby
+# http://ruby.learncodethehardway.org/book/
+#savedir = '~/Ruby_programs/Learn_Ruby_the_Hard_Way'
+#savename = 'Learn_Ruby_the_Hard_Way.pdf'
+#urls = ruby_hard_way_urls
+#css = ['http://ruby.learncodethehardway.org/book/css/syntax.css']
 
-def html_to_pdf(html_file)
-  html = nil
-  savename = html_file.sub(/\.html?$/,'.pdf')
-  unless Dir.entries(".").include?(savename)
-    File.open(html_file, 'r') { |inf| html = inf.read }
-    kit = PDFKit.new(html, :page_size => 'Letter')
-    # we don't want to use these stylesheets
-    #kit.stylesheets << '960.css'
-    #kit.stylesheets << 'screen.css'
-    kit.stylesheets << 'syntax.css'
-    kit.to_file(savename)
-  end
-end
-
-def html_file_array(urls)
-  urls.map { |url| File.basename(url) }
-end
-
-def pdf_file_array(html_file_array)
-  html_file_array.map { |html_file| html_file.sub(/\.html?$/,'.pdf') }
-end
-
-def build_book(html_file_array)
-  unless File.exists?('Learn_Ruby_the_Hard_Way.pdf')
-    pdfs_string = pdf_file_array(html_file_array).join(" ")
-    `pdftk #{pdfs_string} output Learn_Ruby_the_Hard_Way.pdf`
-  end
-end
-
-def delete_temp_files(html_file_array)
-  pdf_file_array(html_file_array).each { |pdffile| File.delete(pdffile) }
-  html_file_array.each { |htmlfile| File.delete(htmlfile) }
-  File.entries(".").match(/\.css$/).each { |cssfile| File.delete(cssfile) }
-end
-
-set_dir(savedir)
-urls = build_urls
-get_css_files
-get_html_files(urls)
-html_file_array = html_file_array(urls)
-html_file_array.each { |html_file| html_to_pdf(html_file) }
-build_book(html_file_array)
-#delete_temp_files(html_file_array)
-
+# Python
+# http://learnpythonthehardway.org/book/
+savedir = '~/Tech/Python/Learn_Python_the_Hard_Way'
+savename = 'Learn_Python_the_Hard_Way.pdf'
+urls = python_hard_way_urls
+css = ['http://learnpythonthehardway.org/book/_static/basic.css']
+      # 'http://learnpythonthehardway.org/book/_static/sphinxdoc.css']
+html_files = HtmlsToPdf.new(savedir,savename,urls,css,false)
+html_files.create_pdf
