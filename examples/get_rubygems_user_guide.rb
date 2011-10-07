514e9fce62f037a69b0d7c38ddba21d41dcb7609
James Lavin <james@jameslavin.com>
2011-10-07 16:44:48 -0400

    update examples/get_rubygems_user_guide.rb

--- examples/get_rubygems_user_guide.rb
+++ examples/get_rubygems_user_guide.rb
@@ -4,6 +4,7 @@ require 'htmls_to_pdf'
 # Get 'RubyGems User Guide' as pdf file
 # Source: 'http://docs.rubygems.org/read/book/1'
 
+config = {}
 config[:urls] = ['http://docs.rubygems.org/read/book/1']
 # I have no idea why these chapters are numbered as they are!
 [1,2,3,4,16,7,5,6,21].each do |val|
@@ -13,4 +14,4 @@ end
 config[:savedir] = '~/Tech/Ruby/GEMS/DOCUMENTATION'
 config[:savename] = 'RubyGems_User_Guide.pdf'
 
-HtmlsToPdf.new(savedir,savename,urls).create_pdf
+HtmlsToPdf.new(config).create_pdf
