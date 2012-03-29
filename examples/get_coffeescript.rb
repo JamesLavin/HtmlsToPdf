56a85505a771f5dbbdf1808687991c7a39cb65f1
James Lavin <james@jameslavin.com>
2012-03-29 13:49:03 -0400

    update get_coffeescript.rb after site moved to coffeescript.org

--- examples/get_coffeescript.rb
+++ examples/get_coffeescript.rb
@@ -2,13 +2,13 @@ require 'rubygems'
 require 'htmls_to_pdf'
 
 # Get 'CoffeeScript_documentation' as pdf file
-# Source: 'http://jashkenas.github.com/coffee-script/'
+# Source: 'http://coffeescript.org/'
 
 config = {}
-config[:urls] = ['http://jashkenas.github.com/coffee-script/']
+config[:urls] = ['http://coffeescript.org/']
 config[:savedir] = '~/Tech/Javascript/COFFEESCRIPT/DOCUMENTATION'
 config[:savename] = 'CoffeeScript_documentation.pdf'
-config[:css] = ['http://jashkenas.github.com/coffee-script/documentation/css/docs.css',
-       'http://jashkenas.github.com/coffee-script/documentation/css/idle.css']
+config[:css] = ['http://coffeescript.org/documentation/css/docs.css',
+       'http://coffeescript.org/documentation/css/idle.css']
 
 HtmlsToPdf.new(config).create_pdf
