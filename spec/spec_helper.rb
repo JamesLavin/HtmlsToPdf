e462fc1e3037516ca21074da75d9f0342453dd9a
James Lavin <james@jameslavin.com>
2012-08-24 13:27:35 -0400

    revert changes to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,8 @@
+require 'fakefs/spec_helpers'
 require 'webmock/rspec'
 
-require 'fakefs'
-# This alternative enables selective inclusion of FakeFS
-# with 'include FakeFS::SpecHelpers'
-#
-# require 'fakefs/spec_helpers'
-# RSpec.configure do |config|
-#   config.include FakeFS::SpecHelpers
-# end
+RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers
+end
 
 Dir["./spec/support/**/*.rb"].each {|f| require f}
